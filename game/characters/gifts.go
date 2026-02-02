8b97e384165e726ad96391d17421602403fc9367
foggez-rgb <ghoul30001@gmail.com>
2026-02-02 09:08:05 +0300

    Create gifts.go

---
+++ game/characters/gifts.go
@@ -0,0 +1,19 @@
+package characters
+
+var Gifts = map[string]Gift{
+	"шоколад": {
+		Name: "шоколад",
+		Affinity: 1,
+		Description: "Сладкий подарок",
+	},
+	"перчатки": {
+		Name: "перчатки",
+		Affinity: 2,
+		Description: "Практичный подарок для боя",
+	},
+	"письмо": {
+		Name: "письмо",
+		Affinity: 3,
+		Description: "Личное и смущающее",
+	},
+}
